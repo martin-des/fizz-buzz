ae4dd1497abb02b5b43d340a5ed0df392f9ffffe
Martin Des <ssymatrin@gmail.com>
2025-04-04 16:45:02 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void fizzbuzz(int n){
+    for(int i=1; i<n ; ++i){
+        if(i%3==0 &&i%5==0){
+            cout<<"Fizzbuss"<<endl;
+        }else if (i%3==0)
+        {
+            cout<<"fizz"<<endl;
+        }else if(i%5==0){
+            cout<<"buss"<<endl;
+        }
+        
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a nummer"<<endl;
+    cin>>n;
+    fizzbuzz(n);
+    return 0;
+}
